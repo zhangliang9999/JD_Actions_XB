eb6e632a095d95f61b7b00039289194179e857e3
ZHANGLIANGzhangliang <48168414+ZHANGLIANGzhangliang@users.noreply.github.com>
2021-01-19 12:22:17 +0800

    Update jd_wechat_sign.js

--- jd_wechat_sign.js
+++ jd_wechat_sign.js
@@ -129,7 +129,6 @@ if (!$.isNode() && typeof $request !=='undefined') {
     })
     if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {
     };
-    if(JSON.stringify(process.env).indexOf('GITHUB')>-1) process.exit(0)
   } else {
     let cookiesData = $.getdata('CookiesJD') || "[]";
     cookiesData = jsonParse(cookiesData);
